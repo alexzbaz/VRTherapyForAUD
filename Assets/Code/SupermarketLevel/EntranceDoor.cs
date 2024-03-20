427b0dde8e85e225a7f5112857b33ac6f0cff932
alexzbaz <alexander.bazhanov@web.de>
2024-03-20 16:53:22 +0100

    Supermarket Design finished

--- Assets/Code/SupermarketLevel/EntranceDoor.cs
+++ Assets/Code/SupermarketLevel/EntranceDoor.cs
@@ -5,55 +5,40 @@ using UnityEngine;
 public class EntranceDoor : MonoBehaviour
 {
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask npcLayer;
     [SerializeField] private GameObject entrance;
-    public ShoppingList shoppingList;
 
     [SerializeField] private bool rightDoor; // Set in Editor
-    [SerializeField] private bool exitDoor; // Set in Editor
     private bool doorOpened = false;
 
 	// Update is called once per frame
 	void Update()
     {
         Collider[] doorColliders = Physics.OverlapSphere(transform.position, 2f, playerLayer);
+        Collider[] npcColliders = Physics.OverlapSphere(transform.position, 1.5f, npcLayer);
 
-        if (!exitDoor)
+        if (doorColliders.Length > 0 || npcColliders.Length > 0)
         {
-            if (doorColliders.Length > 0)
+            if (!doorOpened)
             {
-                if (!doorOpened)
+                doorOpened = true;
+                if (rightDoor)
                 {
-                    doorOpened = true;
-                    if (rightDoor)
-                    {
-                        entrance.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-                    }
-                    else
-                    {
-                        entrance.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                    }
-
+                    entrance.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 }
-            }
-            else
-            {
-                if (doorOpened)
+                else
                 {
-                    entrance.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
-                    doorOpened = false;
+                    entrance.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 }
             }
         }
-
-  //      if (exitDoor)
-		//{
-  //          if (rightDoor)
-		//	{
-  //              if (shoppingList.allItemsInCart)
-  //              {
-  //                  entrance.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-  //              }
-  //          }
-		//}
+        else
+        {
+            if (doorOpened)
+            {
+                entrance.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                doorOpened = false;
+            }
+        }
     }
 }
